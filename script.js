04e97e936ee3ea6e8421b5aaf1aee188effa4b99
Nandini Bhatia <121661905+Nandini53@users.noreply.github.com>
2026-07-01 15:16:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ setInterval(() => {
     current = (current + 1) % slides.length;
     slides[current].classList.add("active");
 }, 5000);
+
+// animation for about company text
+window.addEventListener("scroll", () => {
+    const texts = document.querySelectorAll(".fly-text");
+
+    texts.forEach(text => {
+        const position = text.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight - 100;
+
+        if(position < screenPosition){
+            text.classList.add("show");
+        }
+    });
+});
